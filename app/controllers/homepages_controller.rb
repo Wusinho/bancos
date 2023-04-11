3577d5d648e929150be89e4c6e08de293a8eba13
Heber Lazo <heberlazo@gmail.com>
2023-04-11 13:03:33 -0500

    Added devise error_message on log_in, added navbar when logged_in

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,4 +1,6 @@
 class HomepagesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @welcome = 'Hello User'
   end
--- app/helpers/homepages_helper.rb
+++ app/helpers/homepages_helper.rb
@@ -1,2 +1,9 @@
 module HomepagesHelper
+
+  def navbar_if_signed_in
+    return unless user_signed_in?
+
+    render 'shared/navbar'
+  end
+
 end
