fc546f679ff55b663cb1e6f485c973e59f622328
Heber Lazo <heberlazo@gmail.com>
2023-04-11 19:30:02 -0500

    removed logic from view

--- app/helpers/banks_helper.rb
+++ app/helpers/banks_helper.rb
@@ -1,2 +1,10 @@
 module BanksHelper
+
+  def submit_bank_form(bank)
+    if bank.persisted?
+      button_to('Update Bank', '', class: blue_btn)
+    else
+      button_to('Create Bank', '', class: blue_btn)
+    end
+  end
 end
