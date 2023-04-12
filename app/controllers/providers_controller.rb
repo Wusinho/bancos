7bfe383dd40a0df56d3d476775318e1b152be9a7
Heber Lazo <heberlazo@gmail.com>
2023-04-12 09:08:27 -0500

    Added providers post tests

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -4,5 +4,24 @@ class ProvidersController < ApplicationController
   end
 
   def create
+    @provider = current_user.providers.build(provider_params)
+
+    if @provider.save
+      streams = []
+      streams << turbo_stream.prepend('providers', partial: 'providers/provider', locals: { provider: @provider })
+      streams << turbo_stream.replace('provider_form', partial: 'providers/form', locals: { provider: Provider.new})
+      streams << turbo_stream.replace('message', partial: 'shared/message', locals: { message: "#{@provider.name} created"})
+      render turbo_stream: streams
+    else
+      render turbo_streams: turbo_error_message(instance)
+    end
+
+
+  end
+
+  private
+
+  def provider_params
+    params.require(:provider).permit(:name, :nit, :contact_name, :phone)
   end
 end
--- spec/requests/providers_spec.rb
+++ spec/requests/providers_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-def provider_partial(bank)
+def provider_partial(provider)
   ApplicationController.render(partial: 'providers/provider', locals: { provider: provider }).to_s
 end
 
@@ -18,7 +18,7 @@ RSpec.describe "Providers", type: :request do
         sign_in user
       end
 
-      it 'it include all the current_user banks' do
+      it 'it include all the current_user providers' do
         get providers_path
         card_provider1 = provider_partial(provider1)
         card_provider2 = provider_partial(provider2)
@@ -27,7 +27,7 @@ RSpec.describe "Providers", type: :request do
       end
 
       it 'should not include other users providers' do
-         get banks_path
+         get providers_path
          card_provider3 = provider_partial(provider3)
          expect(response.body).not_to include(card_provider3)
       end
@@ -35,10 +35,59 @@ RSpec.describe "Providers", type: :request do
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/providers/create"
-      expect(response).to have_http_status(:success)
+  describe 'Post /create' do
+    context 'when user is logged in' do
+      let(:provider_params) { FactoryBot.attributes_for(:provider) }
+
+      before do
+        sign_in user
+      end
+
+      it 'creates a new provider' do
+        expect do
+          post providers_path, params: { provider: provider_params }
+        end.to change(provider, :count).by(1)
+      end
+
+      it 'returns a successful response' do
+        post providers_path, params: { provider: provider_params }
+        expect(response).to be_successful
+        provider = Provider.first
+        card = provider_partial(provider)
+        expect(response.body).to include(card)
+      end
+
+      it 'returns the correct turbo stream responses' do
+        post providers_path, params: { provider: provider_params }
+
+        expect(response.body).to include('turbo-stream')
+        expect(response.body).to include('providers')
+        expect(response.body).to include('provider_form')
+
+        expect(response.body).to include('prepend', 'provider')
+        expect(response.body).to include('replace', 'provider_form')
+      end
+
+      it 'renders the error message when no name' do
+        post providers_path, params: { provider: { name: '', nit: provider_params[:nit]  } }
+
+        expect(response.body).to include('error_message','Name is too short (minimum is 3 characters)')
+      end
+
+      it 'renders the error message when nit number is  not 9 digits' do
+        post providers_path, params: { provider: { name: provider_params[:name], nit: '123' } }
+
+        expect(response.body).to include('error_message','nit is the wrong length (should be 15 characters)')
+      end
+    end
+
+    context 'when user is not signed in' do
+      let(:provider_params) { FactoryBot.attributes_for(:provider) }
+
+      it 'redirects to sign in page' do
+        post providers_path, params: { provider: provider_params }
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 
