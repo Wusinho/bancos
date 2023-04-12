6bcb1f7116e4696515d8c37f145059eed4da8e26
Heber Lazo <heberlazo@gmail.com>
2023-04-12 17:18:36 -0500

    Corrected provider card

--- app/helpers/providers_helper.rb
+++ app/helpers/providers_helper.rb
@@ -1,2 +1,25 @@
 module ProvidersHelper
+  def provider_card(provider)
+    content_tag :div, class: "max-w-sm rounded overflow-hidden shadow-lg mb-2" do
+      content_tag :div, class: "px-6 py-4" do
+        concat div_tag(:div,"font-bold text-xl mb-2", provider.name)
+        concat content_tag(:div, provider_information_list(provider), class: "flex items-center")
+        end
+      end
+  end
+
+  def provider_information_list(provider)
+    content_tag(:div, class: "text-sm mr-2") do
+      concat div_tag(:div,"text-gray-900 leading-none", provider.contact_name)
+      concat div_tag(:p, "text-gray-600", provider.phone )
+    end
+  end
+
+  def div_tag(tag, css, provider = 'Sin información en la tabla')
+    content_tag(tag, '', class: css ) do
+      provider
+    end
+
+  end
+
 end
