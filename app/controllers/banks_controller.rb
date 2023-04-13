f692e92157a149342c4d032612c871fc7e450c72
Heber Lazo <heberlazo@gmail.com>
2023-04-13 13:27:44 -0500

    Added pagination to banks

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -4,7 +4,7 @@ class BanksController < ApplicationController
 
   def index
     @bank = Bank.new
-    @banks = current_user.own_banks
+    @banks = current_user.own_banks.paginate(page: params[:page], per_page: 10)
   end
 
   def create
