5b8a212e76c3d1f7e3e239c6a8baa95b16763161
Heber Lazo <heberlazo@gmail.com>
2023-04-12 21:33:10 -0500

    Improved back buttons

--- app/helpers/banks_helper.rb
+++ app/helpers/banks_helper.rb
@@ -7,4 +7,13 @@ module BanksHelper
       button_to('Create Bank', '', class: blue_btn)
     end
   end
+
+  def edit_redirection(bank, action_name)
+    if action_name == 'show'
+      link_to('Back', redirect_to(bank.provider))
+    else
+      link_to('Back', banks_path)
+    end
+  end
+
 end
